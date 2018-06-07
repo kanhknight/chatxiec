b52ea8129589cf2c38f9b58f5c63a4c2384c895e
=Canh Vu <=kanhknight@gmail.com>
2018-06-08 00:53:52 +0700

    thêm chú thích

--- chatxiec/app.py
+++ chatxiec/app.py
@@ -4,9 +4,12 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '123@#@45690@#'
 socketio = SocketIO(app)
 
+#Tin nhắn global 
 @socketio.on('Client-send-message', namespace= '/message')
 def Client_send_message(data):
     print('User {0} gửi tin nhắn!'.format(request.sid))
+
+    # Server gửi tin nhắn global cho tất cả các user
     emit('Server-send-message', data, namespace='/message', broadcast = True)
 
 @app.route('/')
--- chatxiec/static/js/main.js
+++ chatxiec/static/js/main.js
@@ -1,6 +1,8 @@
 var socket_message = io("http://localhost:3000/message");
 
 $(document).ready(function(){
+
+    // Hàm gửi tin nhắn cho server
     $('#text-message').keypress(function(event){
         var keycode = (event.keyCode ? event.keyCode: event.which);
         if(keycode == '13'){
@@ -8,10 +10,10 @@ $(document).ready(function(){
                 socket_message.emit('Client-send-message', $('#text-message').val());
                 $('#text-message').val('');
             }
-                        
         }
     });
 
+    // Nhận tin nhắn server gửi vể
     socket_message.on('Server-send-message', function(data){
         $('#message-container').append('<li>'+ data + '</li>')
     });
