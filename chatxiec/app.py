4fd6f5949a5c354cf70c70a776fd31346dae9426
=Canh Vu <=kanhknight@gmail.com>
2018-06-08 20:09:02 +0700

    Save

--- chatxiec/app.py
+++ chatxiec/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_socketio import *
+from jinja2 import *
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '123@#@45690@#'
 socketio = SocketIO(app)
@@ -10,11 +11,33 @@ def Client_send_message(data):
     print('User {0} gửi tin nhắn!'.format(request.sid))
 
     # Server gửi tin nhắn global cho tất cả các user
-    emit('Server-send-message', data, namespace='/message', broadcast = True)
+    emit('Server-send-message-all-client', data, namespace='/message', broadcast = True)
+
+
+# Xử lý gửi tin nhắn private cho 1 user được chỉ định
+# Bước 1: Xử lý đăng ký user vào Dictionary
+users = {}
+@socketio.on('private-message-send-username', namespace = '/private-mesage')
+def receive_username(username):
+    users[username] = request.sid
+    print(users)
+
+# Bước 2: Xử lý gửi tin nhắn tới user được chỉ định
+@socketio.on('private-message-from-client', namespace = '/private-mesage')
+def receive_private_message(tinnhan):
+    receipient_session_id = users[tinnhan['username']]
+    message = tinnhan['message']
+    print(message)
+    
+    emit('private-message-from-server-receipient', message, room = receipient_session_id)
+    emit('private-message-from-server-sender', message, room = request.sid)
+
+# Kết thúc xử lý tin nhắn private cho user được chỉ định
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 if __name__ == '__main__':
-    socketio.run(app, host= '127.0.0.1', port= 3000, debug = True)
+    socketio.run(app, host= '127.0.0.1', port= 3000, debug = False)
